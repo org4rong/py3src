26c3d6e2c8f233bb54ddec3eafeb90b0382b08a2
Rong Chang <rong@us.ibm.com>
2025-04-20 16:41:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! custom _webchang entries in docker-push.yaml"
+    return "Hello World! custom docker-push.yaml"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
