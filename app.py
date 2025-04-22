e188302c164a8bc208be9dceded5dfdf1b6a46c4
Rong Chang <rong@us.ibm.com>
2025-04-22 12:51:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! use IP address for Concert API endpoint"
+    return "Hello World! use the SaaS Concert of IBM SPS"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
