1bad3636c03b9dfec2b8a8348bf1f0b176fe412f
Rong Chang <rong@us.ibm.com>
2025-04-22 13:16:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! use SPS Concert instance"
+    return "Hello World! SPS Concert instance"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
