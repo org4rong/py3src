0e822a11dcd910c8fcbada3d34a838486e04f199
Rong Chang <rong@us.ibm.com>
2025-04-28 00:03:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! upload package SBOM"
+    return "Hello World! upload SBOM"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
