84383776064a590e8822bf6718ed350e9382f45d
Rong Chang <rong@us.ibm.com>
2025-05-02 19:52:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! tssc templates"
+    return "Hello World! concert"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
