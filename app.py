80cd19ae87cdd3f71e9588daf0a3b9969b414193
Rong Chang <rong@us.ibm.com>
2025-05-03 16:08:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! git filemode"
+    return "Hello World! image url based filtering"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
